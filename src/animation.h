96cd6865516b4437ce7d362c311f7ccd9b500b92
IonutBoraciu <ionut_sorin.boraciu@stud.acs.upb.ro>
2025-02-16 20:07:13 +0200

    executable for the game

--- src/animation.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "common.h"
-#include "map.h"
-void treatAuroraAnimation(SDL_Rect* playerPosition, int up, int down, int left, int right, int frameHeight, int frameWidth, int textureWidth, SDL_Rect* playerRect, int force, int* spX, int* spY, aTime* time, MAP* map,int speed);
-void treatTurretAnimation(PLAYER* mainC, int side, int speedX, int speedY);
--- src/inventory.h
+++
@@ -1,9 +0,0 @@
-#ifndef INVENTORY_H
-#define INVENTORY_H
-#include "common.h"
-#include "life.h"
-#include "map.h"
-
-void inventory(SDL_Rect camera, OBJECTS obj, PLAYER* mainC, SDL_Renderer* render, int widthCurr, int heightCurr, SDL_Texture* map, MAP* mp, int leftRight);
-void addItem(PLAYER* mainC, char location[250], SDL_Renderer* render, char type, int frames, void (*f)(int, ...));
-#endif
